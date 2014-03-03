22e0feac78ec9aab4023a6058c087ce399a8c4aa
rsaundersdev <rsaundersdev@gmail.com>
2014-03-02 21:43:33 -0500

    added code

---
+++ ProjectEuler 3/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+unsigned long long int number = 600851475143LL;
+int highest;
+int prime;
+int i;
+
+int main(){
+    printf("%d\n", number);
+    for(i = 2; i < number/2; i++){
+        printf("%d\n", i);
+        prime = 1;
+        if(!(number%i)){
+            int y;
+            while(prime = 1)
+                for(y = 2; y < i/2; y++)
+                    if(!(i%y)){
+                        printf("\n%d is not prime", i);
+                        prime = 0;
+                        break;
+                    }
+        }
+        if(!prime){
+            printf("\n%d is prime", i);
+            if(i > highest){
+                highest = i;
+                printf("\n%d", highest);
+            }
+        }
+    }
+    printf("The highest prime factor of %d is %d", number, highest);
+    return 0;
+}
+
